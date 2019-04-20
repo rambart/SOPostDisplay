6c74c35052dd44591a33178d86986a2ba3fd16b6
Rambart <Rambart28@yahoo.com>
2019-04-19 21:18:41 -0700

    Initial Commit

---
+++ SOPostDisplay/DataViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DataViewController.swift
+//  SOPostDisplay
+//
+//  Created by Tom on 4/19/19.
+//  Copyright © 2019 Tom. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
